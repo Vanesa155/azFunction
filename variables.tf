883f214bf990bb830049a67e30bc804b48408d29
Vanesa155 <gloriavavi@gmail.com>
2025-02-26 21:03:38 -0500

    trabajo

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "name_function" {
+  type        = string
+  default     = "glorifunction"
+  description = "NameFunction"
+}
+
+variable "location" {
+  type        = string
+  default     = "West Europe"
+  description = "Location"
+}
